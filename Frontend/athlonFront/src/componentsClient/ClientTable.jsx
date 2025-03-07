e5624395900d295b6335dd4d4f7450f1ddd6e95d
Luisa Chavarría <156725126+lumacg06@users.noreply.github.com>
2025-03-07 16:23:24 -0500

    modified ClientTable.jsx in componentsClient

--- Frontend/athlonFront/src/componentsClient/ClientTable.jsx
+++ Frontend/athlonFront/src/componentsClient/ClientTable.jsx
@@ -1,33 +1,37 @@
 import ClientRow from "./ClientRow";
 
-function ClientTable ({clients,onEdit,onDelete}) {
-
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>Email</th>
-                        <th>Fecha de Registro</th>
-                        <th>Fecha de Vencimiento</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {clients && clients.length > 0 ? (
-                        clients.map ((client) => (
-                            <ClientRow key={client.clienteID} client={client} onEdit={onEdit} onDelete={onDelete}/>
-                        ))
-                    ) : (
-                        <tr>
-                            <td colSpan={'6'}>No hay clientes</td>
-                        </tr>  
-                    )}
-                </tbody>
-            </table>
-        </div>
-    );
+function ClientTable({ clients, onEdit, onDelete }) {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>Fecha de Registro</th>
+            <th>Fecha de Vencimiento</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clients && clients.length > 0 ? (
+            clients.map((client) => (
+              <ClientRow
+                key={client.clienteID}
+                client={client}
+                onEdit={onEdit}
+                onDelete={onDelete}
+              />
+            ))
+          ) : (
+            <tr>
+              <td colSpan={"6"}>No hay clientes</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ClientTable;
