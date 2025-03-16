8a2b80ca0a9ea790232a5f1b323674beb1adb335
JarbeyOcampo1 <164644585+JarbeyOcampo1@users.noreply.github.com>
2025-03-16 12:08:54 -0500

    ADD PIE PAGINA Y INCONO

--- Frontend/athlonFront/src/Major.jsx
+++ Frontend/athlonFront/src/Major.jsx
@@ -4,6 +4,7 @@ import imagen1 from "./imagenes/imagen1.jpg";
 import imagen2 from "./imagenes/imagen2.jpg";
 import imagen3 from "./imagenes/imagen3.jpg";
 import logo from "./imagenes/logo.png";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 function Major() {
   return (
@@ -11,7 +12,6 @@ function Major() {
       <div className="Container_Major">
         <div className="logo-container">
         <img src={logo} alt="Logo Gimnasio Atlhon" className="principal-logo-image" />
-        
         </div>
         <nav className="navbar">
           <Link to="/client">Clientes</Link>
@@ -20,7 +20,8 @@ function Major() {
         </nav>
       </div>
       <div className="title-container">
-      <h1 class="title"> <span>G</span><span>I</span><span>M</span><span>N</span><span>A</span><span>S</span><span>I</span><span>O</span> </h1>
+      <h1 className="title"> <span>G</span><span>I</span><span>M</span><span>N</span><span>A</span><span>S</span><span>I</span><span>O</span>
+      &nbsp;&nbsp;&nbsp;<span>A</span><span>T</span><span>H</span><span>L</span><span>O</span><span >N</span></h1>
       </div>
       <div className="carrousel">
         <div><img src={imagen1}  alt="imagen1"/></div>
@@ -36,6 +37,14 @@ function Major() {
         <div><img src={imagen2} alt="imagen2"/></div>
         <div><img src={imagen3} alt="imagen3"/></div>
       </div>
+      <footer className="footer">
+        <h2>Síguenos en nuestras redes sociales:</h2>
+        <div className="social-icons">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+        </div>
+      </footer>
     </div>
   );
 }
