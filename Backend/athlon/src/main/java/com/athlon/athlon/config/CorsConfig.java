5f213830f8eb47cebcef72113896c726c33e9c86
Luisa Chavarría <156725126+lumacg06@users.noreply.github.com>
2025-03-16 02:32:57 -0500

    Agregar anotación @NonNull en el método addCorsMappings de CorsConfig

--- Backend/athlon/src/main/java/com/athlon/athlon/config/CorsConfig.java
+++ Backend/athlon/src/main/java/com/athlon/athlon/config/CorsConfig.java
@@ -4,17 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull; // Import the NonNull annotation
 
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) { // Add @NonNull annotation here
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173/")//URL del front
-        .allowedMethods("*")//metodos permitidos desde el front
-        .allowCredentials(true);//permitir credenciales
+            .allowedOrigins("http://localhost:5173/") // URL del front
+            .allowedMethods("*") // Metodos permitidos desde el front
+            .allowCredentials(true); // Permitir credenciales
     }
-    
 }
