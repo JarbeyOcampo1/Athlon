c0e217b58dfe264e335f469d410552361003c8b5
Luisa Chavarría <156725126+lumacg06@users.noreply.github.com>
2025-03-17 23:17:15 -0500

    Formatear el código en App.jsx para mejorar la legibilidad

--- Frontend/athlonFront/src/App.jsx
+++ Frontend/athlonFront/src/App.jsx
@@ -1,17 +1,17 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css'
-import LoginsForm from './components/componentsLogin/LoginsForm'
-import Logins from './components/componentsLogin/Logins'
-import Client from './components/componentsClient/Client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import LoginsForm from "./components/componentsLogin/LoginsForm";
+import Logins from "./components/componentsLogin/Logins";
+import Client from "./components/componentsClient/Client";
 
 function App() {
-  
-
-  return <BrowserRouter>
-    <Routes>
-      <Route index path="/" element={<Client/>}/>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Client />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
